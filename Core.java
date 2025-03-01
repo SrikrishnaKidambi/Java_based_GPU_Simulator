415abe28e5b7dd0bacd3fb52ba53f91662042ab4
SrikrishnaKidambi <kidambivenkatasrikrishna@gmail.com>
2025-03-01 09:51:24 +0530

    Trying to divide the stages of pipeline

--- Core.java
+++ Core.java
@@ -1,4 +1,47 @@
+import java.util.Map;
 
 public class Core {
+    public int[] registers;
+    public int pc;
+    public int coreID;
+    private String a_0=""; // variable used for loading the string to be printed using ecall
+    public int cc;
 
+    public Core(int coreID){
+        this.coreID=coreID;
+        this.pc=0;
+        this.registers=new int[32];
+        registers[0]=0;
+        this.cc=0;
+    }
+
+    public void execute(String[] program,Map<String,Integer>labelMapping,Memory mem,Map<String,String>stringVariableMapping,Map<String,Integer>nameVariableMapping){
+
+    }
+    private String instructionParser(String instruction) {
+	  
+        int hashSymbolIdx=instruction.indexOf("#");
+        if(hashSymbolIdx!=-1 && hashSymbolIdx==0) {
+            if(hashSymbolIdx>0 && instruction.charAt(hashSymbolIdx-1)!=' ') {
+                throw new IllegalArgumentException("Invalid comment. Space is missing after the instruction");
+            }
+            return instruction.substring(0,hashSymbolIdx).trim();
+        }
+        return instruction.trim();
+    }
+    private String IF(String[] program){
+        return program[pc++];
+    }
+    private void ID_RF(String instruction){
+        
+    }
+    private void EX(){
+
+    }
+    private void MEM(){
+
+    }
+    private void WB(){
+
+    }
 }
